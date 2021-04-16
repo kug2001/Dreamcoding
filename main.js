9bc21732983c64f118d042552b3a024665bc5aa3
young <kug2001@gmail.com>
2021-04-16 20:59:49 +0900

    javascript contact btn

--- main.js
+++ main.js
@@ -27,8 +27,20 @@ navbarMenu.addEventListener('click', (event) =>{
     if (link == null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 });
 
+//hande scrolling when tapping on the 'Contact me' button
+
+const contactBtn = document.querySelector('.home__contact');
+
+contactBtn.addEventListener('click', (event) =>{
+    const target = event.target;
+    const link = target.dataset.link;
+    scrollIntoView(link);
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+};
